0ec75dbccc9937962b4512ca5155a95726f55377
mpgprofe <mpgprofe@gmail.com>
2020-11-18 10:30:02 +0100

    Primera versión

---
+++ app/src/main/java/com/example/basesdedatossql/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.basesdedatossql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextID, editTextModelo, editTextMarca, editTextPrecio;
+    Button buttonGuardar, buttonBorrar, buttonMostrar;
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextID = findViewById(R.id.editTextId);
+        editTextModelo = findViewById(R.id.editTextModelo);
+        editTextMarca = findViewById(R.id.editTextMarca);
+        editTextPrecio = findViewById(R.id.editTextPrecio);
+        buttonGuardar = findViewById(R.id.buttonGuardar);
+        buttonBorrar = findViewById(R.id.buttonBorrar);
+        buttonMostrar = findViewById(R.id.buttonMostrar);
+        listView = findViewById(R.id.lista);
+
+        ManejadorBD manejadorBD = new ManejadorBD(this);
+
+
+        buttonMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor cursor = manejadorBD.listar();
+                ArrayAdapter<String> adapter;
+                List<String> lista = new ArrayList<>();
+
+                if ((cursor != null) && cursor.getCount() > 0) {
+                    while (cursor.moveToNext()) {
+                        String fila = "";
+                        fila += "ID: " + cursor.getString(0);
+                        fila += " MODELO: " + cursor.getString(1);
+                        fila += " MARCA: " + cursor.getString(2);
+                        fila += " PRECIO: " + cursor.getString(3);
+                        lista.add(fila);
+                    }
+                    adapter = new ArrayAdapter<>(getApplicationContext(),
+                            R.layout.support_simple_spinner_dropdown_item, lista);
+                    listView.setAdapter(adapter);
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Vacío", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+        buttonGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean resultado = manejadorBD.insertar(editTextModelo.getText().toString(),
+                        editTextMarca.getText().toString(), editTextPrecio.getText().toString());
+
+                if (resultado) {
+                    Toast.makeText(MainActivity.this, "Insertado portátil OK", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Error en la inserción.", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+        buttonBorrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean borrado = manejadorBD.borrar(editTextID.getText().toString());
+                if (borrado) {
+                    Toast.makeText(MainActivity.this, "Borrado Correctamente", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Nada a borrar.", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/basesdedatossql/ManejadorBD.java
@@ -0,0 +1,73 @@
+package com.example.basesdedatossql;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class ManejadorBD extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "moviles.db";
+
+    private static final String COL_ID = "ID";
+    private static final String COL_MODELO = "MODELO";
+    private static final String COL_MARCA = "MARCA";
+    private static final String COL_PRECIO = "PRECIO";
+
+    private static final String TABLE_NAME = "MOVILES";
+
+    public ManejadorBD(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    public ManejadorBD(MainActivity mainActivity) {
+        super(mainActivity, DATABASE_NAME, null, 1);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_NAME + "(" + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COL_MODELO + " TEXT, " + COL_MARCA + " TEXT, " + COL_PRECIO + " TEXT " + ")");
+
+
+    }
+
+    public boolean insertar(String modelo, String marca, String precio) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_MARCA, marca);
+        contentValues.put(COL_MODELO, modelo);
+        contentValues.put(COL_PRECIO, precio);
+
+        long resultado = db.insert(TABLE_NAME, null, contentValues);
+        db.close();
+        return (resultado != -1);
+
+    }
+
+    boolean borrar(String id) {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
+        int borradas = sqLiteDatabase.delete(TABLE_NAME, COL_ID + "=?", new String[]{id});
+
+        sqLiteDatabase.close();
+
+        return (borradas > 0);
+    }
+
+    Cursor listar(){
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM "+TABLE_NAME,null);
+        return cursor;
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
